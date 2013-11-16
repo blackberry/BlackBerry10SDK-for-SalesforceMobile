7b9653e9b8b7c8aa010ee4e10d285a7b5fcacdaf
Livan Yi Du <Livan Yi Du@192.168.0.197>
2013-11-15 19:43:21 -0500

    ld - completed SFRestAPI class documentation

--- SalesforceSDK/include/rest/SFRestAPI.h
+++ SalesforceSDK/include/rest/SFRestAPI.h
@@ -27,7 +27,7 @@ class SFOAuthInfo;
 
 /*!
  * @class SFRestAPI
- * @headerfile SFRestAPI.h "SFRestAPI.h" TODO update according to deployment
+ * @headerfile SFRestAPI.h <rest/SFRestAPI.h>
  * @brief The SFRestAPI is the main class to send REST requests to Force.com REST API
  *
  * @details
@@ -146,33 +146,33 @@ public:
 	/*! Creates a @c SFRestRequest which lists summary information about each Salesforce.com version currently available.
 	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_versions.htm
 	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForVersions();
 
 	/*! Creates a @c SFRestRequest which lists available resources for the current configured API version, including resource name and URI.
 	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_discoveryresource.htm
 	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForResources();
 
 	/*! Creates a @c SFRestRequest which lists the available objects and their metadata for your organization's data.
 	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_describeGlobal.htm
 	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForDescribeGlobal();
 
 	/*! Creates a @c SFRestRequest which lists the summary of the individual metadata for the specified object.
 	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_sobject_basic_info.htm
 	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
 	 * @param objectType String of the object type. Example: "Account"
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForMetadata(const QString & objectType);
 
 	/*! Creates a @c SFRestRequest which completely describes the individual metadata for the sepcified object.
 	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_sobject_describe.htm
 	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
 	 * @param objectType String of the object type. Example: "Account"
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForDescribeObject(const QString & objectType);
 
 	/*! Creates a @c SFRestRequest which retrieves field values for a record of the given type.
@@ -181,19 +181,80 @@ public:
 	 * @param objectType String of the object type. Example: "Account"
 	 * @param objectId The object's object ID.
 	 * @param fieldList The list of fields for which to return values. Example: ["Name", "BillingCity", "CustomField__c"]
-	 * @return pointer to created SFRestRequest. */
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForRetrieveObject(const QString & objectType, const QString & objectId, const QStringList & fieldList);
+
+	/*! Creates a @c SFRestRequest which creates a new record of the given type.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/dome_sobject_create.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param objectType String of the object type. Example: "Account"
+	 * @param fields A QVariantMap containing initial field names and values for the record.
+	 * For example, {Name: "salesforce.com", TickerSymbol: "CRM"}
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForCreateObject(const QString & objectType, const QVariantMap & fields);
+
+	/*! Creates a @c SFRestRequest which updates field values on a record of the given type.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/dome_update_fields.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param objectType String of the object type. Example: "Account"
+	 * @param objectId The object's object ID.
+	 * @param fields A QVariantMap containing initial field names and values for the record.
+	 * For example, {Name: "salesforce.com", TickerSymbol: "CRM"}
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForUpdateObject(const QString & objectType, const QString & objectId, const QVariantMap & fields);
+
+	/*! Creates a @c SFRestRequest which creates or updates record of the given type, based on the given external ID.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_sobject_upsert.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param objectType String of the object type. Example: "Account"
+	 * @param extIdField external ID field name. For example, "accountMaster__c"
+	 * @param extIdValue the record's external ID value
+	 * @param fields A QVariantMap containing initial field names and values for the record.
+	 * For example, {Name: "salesforce.com", TickerSymbol: "CRM"}
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForUpsertObject(const QString & objectType, const QString & extIdField, const QString & extIdValue, const QVariantMap & fields);
+
+	/*! Creates a @c SFRestRequest which deletes a record of the given type.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/dome_delete_record.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param objectType String of the object type. Example: "Account"
+	 * @param objectId The object's object ID.
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForDeleteObject(const QString & objectType, const QString & objectId);
+
+	/*! Creates a @c SFRestRequest which executes the specified SOQL query.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_query.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param soql A string containing the query to execute. For example, "SELECT Id,Name from Account ORDER BY Name LIMIT 20"
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForQuery(const QString & soql);
+
+	/*! Creates a @c SFRestRequest which executes the specified SOSL search.
+	 * @see http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_search.htm
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param sosl a string containing the search to execute. For example, "FIND {needle}"
+	 * @return the pointer to the created SFRestRequest. */
 	Q_INVOKABLE sf::SFRestRequest * requestForSearch(const QString & sosl);
 
 	/*
 	 * NOTE: Because QScriptEngine is not aware of enums, it cannot correctly interpret the signature.
-	 * 		So this function should be only used in C++
+	 * 		Therefore this function should be only used in C++
 	 */
+	/*! Creates a custom @c SFRestRequest. This function ignores values in @c SFRestAPI::endPoint and @c SFRestAPI::apiVersion.
+	 * @note Because QScriptEngine is not aware of enums, it cannot correctly interpret the signature.
+	 * Therefore this function should be only used in C++.
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param path Relative path to your application's instance URL, or absolute path of your HTTP URL. If you use absolute path, you need to include
+	 * the scheme.
+	 * @param method HTTP verb, GET, POST, HEAD, DELETE, etc. Please see @c HTTPMethod::Type for more details.
+	 * @param params The key-value pairs you want included in your REST request. You don't need to escape HTTP reserved characters.
+	 * @param contentType Indicate how to encode key-value pairs stored in @a params. If your HTTP verb are GET, HEAD or DELETE
+	 * This the contentType is should be @c SFRestRequest::HTTPContentTypeUrlEncoded. Please see @c SFRestRequest::HTTPContentType for more details.
+	 * @note SFRestRequest::HTTPContentTypeMultiPart is not supported in SDK version <em>1.0</em>.
+	 * @return the pointer to the created SFRestRequest.
+	 * @sa SFRestAPI::customRequest(const QString&,const QScriptValue&,const QVariantMap &,const QScriptValue&),
+	 * HTTPMethod::Type,
+	 * SFRestRequest::HTTPContentType*/
 	Q_INVOKABLE sf::SFRestRequest * customRequest(
 			const QString & path,
 			const HTTPMethodType & method = HTTPMethod::HTTPGet,
@@ -201,6 +262,18 @@ public:
 			const sf::SFRestRequest::HTTPContentType & contentType = sf::SFRestRequest::HTTPContentTypeJSON);
 
 	/* NOTE: This is the corresponding QML version of API, it internally calls the C++ version to create the request */
+	/*! Creates a custom @c SFRestRequest. This is the corresponding QML version of the API.
+	 * @remark The returned object has parent set to 0. If the method is called in C++, it's your responsibility to manage the memory. If the method is called from QML, the ownership is automatically transfered to the JavaScript engine.
+	 * @param path Relative path to your application's instance URL, or absolute path of your HTTP URL. If you use absolute path, you need to include
+	 * the scheme.
+	 * @param method A @c QScriptValue containing an integer that matches the values of @c HTTPMethod::Type.
+	 * @param params The key-value pairs you want included in your REST request. You don't need to escape HTTP reserved characters.
+	 * @param contentType A @c QScriptValue containing an integer that matches the values of @c SFRestRequest::HTTPContentType.
+	 * @note SFRestRequest::HTTPContentTypeMultiPart is not supported in SDK version <em>1.0</em>.
+	 * @return the pointer to the created SFRestRequest.
+	 * @sa SFRestAPI::customRequest(const QString&,const HTTPMethodType&,const QVariantMap &,const sf::SFRestRequest::HTTPContentType &),
+	 * HTTPMethod::Type,
+	 * SFRestRequest::HTTPContentType*/
 	Q_INVOKABLE sf::SFRestRequest * customRequest(
 				const QString & path,
 				const QScriptValue & method,
@@ -208,6 +281,15 @@ public:
 				const QScriptValue & contentType);
 
 	/* Convenience APIs */
+	/*! Generate a SOQL query.
+	 * @param fields - @c QStringList of fields to select. e.g. ["Id", "CustomField__c"]
+	 * @param sObject - object type to query. e.g. "Account"
+	 * @param where - WHERE clause. e.g. "Name='Salesforce'"
+	 * @param groupBy - @c QStringList of GROUP BY fields
+	 * @param having - HAVING clause
+	 * @param orderBy - @c QStringList of ORDER BY fields
+	 * @param limit - number for LIMIT clause. The LIMIT clause is not added if not added if give 0
+	 * @return Generated SOQL query */
 	Q_INVOKABLE QString generateSOQLQuery(const QStringList & fields, const QString & sObjectType,
 			const QString & where = "",
 			const QStringList & groupBy = QStringList(),
@@ -215,14 +297,25 @@ public:
 			const QStringList & orderBy = QStringList(),
 			const int & limit = 0);
 
+	/*!
+	 * Generate a SOSL search string.
+	 * @param term - the search term. e.g. "John*"
+	 * @param fieldScope - the SOSL scope, e.g. "IN ALL FIELDS". If empty, defaults to "IN NAME FIELDS"
+	 * @param objectScope - a @c QVariantMap where each key is an sObject name and each value is a string
+	 * with the fieldlist and (optional) where, order by, and limit clause for that object.
+	 * Set empty to search all searchable objects.
+	 * @param limit - overall search limit (max 200)
+	 * @return Generated SOSL search string */
 	Q_INVOKABLE QString generateSOSLSearchQuery(const QString & searchTerm,
 			const QString & fieldScope = "",
 			const QVariantMap & objectScope = QVariantMap(),
 			const int & limit = 0);
 
+	/*! @param searchTerm The search term to be sanitized.
+	 * @return SOSL-safe version of search term */
 	Q_INVOKABLE QString sanitizeSOSLSearchTerm(const QString & searchTerm);
 
-	//TODO add more
+	//More APIs
 
 private:
 	/* private constructor, Singleton */
