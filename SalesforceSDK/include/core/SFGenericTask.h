2135611fedcdea910314cddfca25a7edec820cd3
Livan Yi Du <Livan Yi Du@192.168.0.197>
2013-12-02 15:20:56 -0500

    ld - Documentation for SFGenericTask, SFNetworkAccessTask

--- SalesforceSDK/include/core/SFGenericTask.h
+++ SalesforceSDK/include/core/SFGenericTask.h
@@ -34,68 +34,134 @@ class SFResult;
 
 using namespace Qt;
 
+/*!
+ * @class SFGenericTask
+ * @headerfile SFGenericTask.h <core/SFGenericTask.h>
+ * @brief An abstract class that defines a concurrent Task-Oriented logic block
+ *
+ * @details The purpose of this class is to establish a task-oriented logic block which can be easily executed concurrently. The class inherits
+ * from @c QRunnable which can be executed in separate thread. It also implemented @c QObject features in order to take the advantage of
+ * event loop and signal-slot architecture.
+ *
+ * The class defined numbers of properties and signals for reporting progress and controlling work flow. It also provides various of virtual
+ * functions for subclass to override. In addition, the class implemented numbers of convenient functions to create/manage objects that
+ * is commonly used in multi-threading environment.
+ *
+ * To create a simple concurrent task, all you need to do is to create a subclass of @c SFGenericTask and implement the @c execute(). Within your
+ * @c execute() function, make sure you populate the @c SFGenericTask::mResult object and return proper status code.
+ *
+ * When you want to execute your concurrent task, create an instance of your subclass and call @c startTaskAsync(). The base class implementation
+ * makes sure that the logic in @c execute() is executed in separate thread and the result object you created in that function is moved to
+ * correct thread when it's delivered.
+ *
+ * It is up to developers on how to use this class. @c SFNetworkAccessTask is a concrete implementation of this class.
+ * It demonstrates that you could create a much more complex operation out of @c SFGenericTask by overriding functions.
+ *
+ * @see SFResult, SFNetworkAccessTask, SFRestResourceTask
+ *
+ * \author Livan Yi Du
+ */
 class SFGenericTask: public QObject, public QRunnable {
 	Q_OBJECT
 	Q_ENUMS(TaskStatus)
-	Q_PROPERTY(TaskStatus status READ status);
-	Q_PROPERTY(sf::SFResult* result READ result);
-	Q_PROPERTY(QVariantHash tags READ tags);
-	Q_PROPERTY(bool cancellable READ isCancellable WRITE setCancellable);
-	Q_PROPERTY(bool autoRetry READ isAutoRetry WRITE setAutoRetry);
+	Q_PROPERTY(TaskStatus status READ status) /*!< The current status of the task. @sa SFGenericTask::mStatus, SFGenericTask::TaskStatus */
+	Q_PROPERTY(sf::SFResult* result READ result) /*!< The result object of this task @sa result(), SFGenericTask::mResult*/
+	Q_PROPERTY(QVariantHash tags READ tags) /*!< A hash map of any additional objects associated with the task. @sa tags(), putTag() */
+	Q_PROPERTY(bool cancellable READ isCancellable WRITE setCancellable) /*!< Whether this task can be cancelled. Default is @a false. @sa isCancellable(), setCancellable() */
+	Q_PROPERTY(bool autoRetry READ isAutoRetry WRITE setAutoRetry) /*!< Whether the auto-retry is enabled. @sa setAutoRetry(), isAutoRetry() */
 
 signals:
-	void taskPreExecution(sf::SFGenericTask*);
-	void taskFinished(sf::SFGenericTask*);
-	void taskResultReady(sf::SFResult*);
-	void taskShouldRetry(sf::SFGenericTask*, sf::SFResult*);
-	void taskDidAutoRetry(sf::SFGenericTask*);
+	/*! Emitted before the @c execute() is invoked.
+	 * @param task the task that emitted the signal. */
+	void taskPreExecution(sf::SFGenericTask* task);
+	/*! Emitted after the task is finished.
+	 * @param task the task that is finished. @note The implementation only guarantee that the task object is valid at the time when
+	 * slots are invoked. @c deleteLater() is called after the signal is emitted. So attempting to dereference the pointer at later
+	 * time may cause memory access error.*/
+	void taskFinished(sf::SFGenericTask* task);
+	/*! Emitted when result is ready
+	* @param result the result object. @note the object is only valid within the scope of connected slot. It will be deleted when
+	* all connected slots are finished. So you have to collect all data before the @a result object get deallocated. */
+	void taskResultReady(sf::SFResult* result);
+	/*! Emitted when manual retry is required. It's connected slot's responsibility to re-start the task or delete the task.
+	 * @param task the task that want to be re-started
+	 * @param result the object that contains the reason of re-trying.*/
+	void taskShouldRetry(sf::SFGenericTask* task, sf::SFResult* result);
+	/*! Emitted when auto retry happened
+	 * @param task the task that re-started itself*/
+	void taskDidAutoRetry(sf::SFGenericTask* task);
 public:
+	/*! Possible status */
 	enum TaskStatus {
-		TaskStatusNotStarted = -2,
-		TaskStatusRunning = -1,
-		TaskStatusFinished= 0,
-		TaskStatusError = 1,
-		TaskStatusCancelled = 2,
-		TaskStatusWillRetry = 3,
+		TaskStatusNotStarted = -2, /*!< The task is not started yet */
+		TaskStatusRunning = -1, /*!< The task is executing */
+		TaskStatusFinished= 0, /*!< The task is finished without error */
+		TaskStatusError = 1, /*!< The task is finished with error */
+		TaskStatusCancelled = 2, /*!< The task is canceled */
+		TaskStatusWillRetry = 3, /*!< The task will be re-started */
 	};
+	/*! @param cancellable whether the task can be canceled */
 	SFGenericTask(const bool & cancellable = false);
+	/*! Default destructor */
 	virtual ~SFGenericTask();
+	/*! Entry point of concurrent execution. Subclass should not call this function directly. */
 	void run();
+	/*! @return status of the task */
 	TaskStatus status() {return mStatus;};
+	/*! @return the result object of the task */
 	SFResult *result() {return mResult;};
+	/*! @return the hash map of all additional objects associated with this task. @sa putTag()*/
 	QVariantHash & tags() { return mTags;};
+	/*! @return Whether this task can be canceled. @sa setCancellable(), SFGenericTask::cancellable */
 	bool isCancellable() {return mCancellable;};
+	/*! Set whether this task can be canceled. This flag should be set before the task is started. @sa isCancellable(), SFGenericTask::cancellable */
 	void setCancellable(const bool & cancellable);
+	/*! @return Whether auto-retry is enabled. @sa setAutoRetry(), SFGenericTask::autoRetry */
 	bool isAutoRetry() {return mAutoRetry;};
+	/*! Set whether auto-retry is enabled. If enabled, the task would re-start itself and decrement @c SFGenericTask::retryCount() by 1, whenever error occurred. @sa isAutoRetry(), SFGenericTask::autoRetry */
 	void setAutoRetry(const bool & autoRetry) {this->mAutoRetry = autoRetry;};
+	/*! @return how many re-try attempts are allowed. @sa setRetryCount() */
 	int retryCount() {return mRetryCount;};
+	/*! Set how many re-try attempts are allowed. Setting this property after the task started result in undefined behavior. @sa retryCount()*/
 	void setRetryCount(const int & retryCount) {this->mRetryCount = retryCount;};
+	/*! Add an @c QVariant to the task and associate it with a key. @sa tags(), SFGenericTask::tags */
 	void putTag(const QString & key, const QVariant & tag) {mTags[key] = tag;};
 
 public slots:
+	/*! Start the task asynchronously and connect a result handler to it */
 	virtual void startTaskAsync(QObject* resultReceiver = NULL, const char * resultReceiverSlot = NULL);
+	/*! Request to cancel the task. If @c SFGenericTask::cancellable is set to false, calling this function have no effect */
 	virtual void cancel();
 
 protected:
-	volatile TaskStatus mStatus;
-	SFResult *mResult;
-	QVariantHash mTags;
-	QThread * mExecutionThread;
-	bool mCancellable;
-	bool mAutoRetry;
-	int mRetryCount;
-	QEventLoop* eventLoop();
-	QMutex *mutex();
-	void setCancelled(const bool &cancelled);
-	bool isCancelled();
+	volatile TaskStatus mStatus; /*!< Current task status */
+	SFResult *mResult; /*!< The result object */
+	QVariantHash mTags; /*!< A hash map holds all objects that related to the task. This collection will be transfered to @c SFResult at the end of the task. */
+	QThread * mExecutionThread; /*!< The thread that execute the task. */
+	bool mCancellable; /*!< flag of whether the task can be canceled */
+	bool mAutoRetry; /*!< flag of whether the task should automatically re-try */
+	int mRetryCount; /*!< allowed number of re-try attempts */
+	QEventLoop* eventLoop(); /*!< Create or re-use a @c QEventLoop. @return For each task, it guarantees to return the same event loop object. */
+	QMutex *mutex(); /*!< Create or re-use a @c QMutex. @return For each task, it guarantees to return the same mutex object. */
+	void setCancelled(const bool &cancelled); /*!< Set a flag that indicating the task is requested to cancel. This is a thread-safe function. @sa isCancelled() */
+	bool isCancelled(); /*!< @return whether the task is requested to cancel. Subclass should constantly check this flag if it support canceling. This is a thread-safe function. @sa setCancelled() */
 
+	/*! Called before the task is added to thread pool. The base implementation responsible to connect result handling slot
+	 * and set necessary properties for proper memory management. Subclass should always call base implementation.
+	 * @param receiver The QObject that will handle the result
+	 * @param slot The Qt slot that respond to @c taskResultReady(sf::SFResult*). The string should be generated using macro @c SLOT()*/
 	virtual void prepareToStart(QObject* receiver, const char * slot);
-	virtual void prepare();
+	virtual void prepare(); /*!< Called in execution thread before the @c execute(). */
+	/*! Pure member function. The subclass should override this function. Guaranteed called in execution thread and is try-catch protected
+	 * @return the status of the task after the execution. Please see @c SFGenericTask::TaskStatus for details.*/
 	virtual TaskStatus execute() = 0;
-	virtual void cleanup();
-	virtual bool retry();
+	virtual void cleanup(); /*!< Called after the @c execute(). The base implementation is responsible to clean up the memory, re-try and emit signals. Subclass should always call base.*/
+	virtual bool retry(); /*!< Called when retry is necessary. This function is responsible to reset the task for re-start. @return whether the task is re-started.*/
 
-	/* this function assume the given object lives in current executing thread */
+	/*! This is a convenient function to push the object to task object's thread and set the task object as the parent. After calling this function.
+	 * it's safe to remove references to the given object without potential memory leak. @note This function is usually called in execution thread, and the
+	 * function assume the given object was created in the current executing thread.
+	 * @param object the object you want to get rid of.*/
 	void prepareQObjectForDisposal(QObject* object);
 private:
 	QEventLoop *mEventLoop;
--- SalesforceSDK/include/core/SFNetworkAccessTask.h
+++ SalesforceSDK/include/core/SFNetworkAccessTask.h
@@ -32,87 +32,228 @@ class QTimer;
 
 namespace sf {
 
+/*!
+ * @class SFNetworkAccessTask
+ * @headerfile SFNetworkAccessTask.h <core/SFNetworkAccessTask.h>
+ * @brief A class for performing basic HTTP/HTTPS transactions.
+ *
+ * @details This class is a concrete implementation of abstract class @c SFGenericTask. It's designed to send HTTP request and receive response
+ * asynchronously and concurrently. The purpose of this class is to provide a base implementation for any complex network transactions.
+ * The class is implemented using Finite-State-Machine(FSM) approach and is capable of running any response processing
+ * in a separated thread from QThreadPool.
+ *
+ * Usage
+ * ------
+ * For most REST requests with Force.com, developer should use @c SFRestResourceTask which is a subclass of this. However, this class
+ * can be used directly for any non-RESTful transactions:
+ * @code
+ * //Prepare HTTP request
+ * QNetworkRequest request("https://www.webservice.com/path/to/the/service");
+ * QByteArray body;
+ *
+ * //Configure your request...
+ *
+ * //Prepare the task
+ * SFNetworkAccessTask *task = new SFNetworkAccessTask(getSharedNetworkAccessManager(), request, HTTPMethod::HTTPPost);
+ * task->setRequestBytesArray(body.toUtf8());
+ *
+ * //Enable cancel
+ * task->setCancellable(true);
+ * connect(pCancelButton, SIGNAL(clicked()), task, SLOT(cancel()));
+ *
+ * //Connect slots, schedule and run
+ * task->startTaskAsync(this, SLOT(onNetworkReplyReady(sf::SFResult*)));
+ * @endcode
+ * And in your slot:
+ * @code
+ * void SFOAuthCoordinator::onNetworkReplyReady(sf::SFResult* result) {
+ * 	if (result->hasError()) {
+ * 		//Handle error
+ * 	} else {
+ * 		QNetworkReply *reply = result->payload<QNetworkReply*>();
+ * 		if (reply) {
+ * 			//Handle the reply
+ * 		}
+ * 	}
+ * }
+ * @endcode
+ *
+ * @anchor SFNetworkAccessTask_Subclass
+ * Subclassing Notes
+ * ------------------
+ *
+ * Typically, for any Force.com REST API, you should use @c SFRestResourceTask. In addition, the source code of @c SFRestResourceTask
+ * provides a good example of subclassing @c SFNetworkAccessTask. You can add your custom logic by overriding following
+ * functions:
+ * - @c prepare() called as soon as the task is started. The function is executed in main thread (or the thread this task belongs to)
+ *
+ * - @c cleanup() called when the task is finished and ready to deliver the result. The base class implementation does the memory cleanup
+ * and emits signals in proper order. This function also attempt to re-try if enabled. So please call base class implementation at the end of your custom logic.
+ * This function can be executed in any thread.
+ *
+ * - @c retry() called when auto re-try is triggered. This function reset the task and cleanup memory allocated during the operation.
+ * This function is invoked from @c cleanup(), so it can be executed in any thread.
+ *
+ * - @c ensureRequest() called before the request is sent. It is executed only in the task's owning thread. Please see @c ensureRequest()
+ * for possible returned states.
+ *
+ * - @c preprocessReply(QNetworkReply*) called when each @c QNetworkReply finished. It is executed only in the task's owning thread.
+ * The main responsibility of this function is to handles HTTP redirect or caching. Please see @c preprocessReply(QNetworkReply*)
+ * for possible returned states.
+ *
+ * - @c processReply(QNetworkReply*) called when the reply is ready to be processed. The function is always executed in a separate thread.
+ * Please see @c processReply(QNetworkReply*) for possible returned states.
+ *
+ * @see SFResult, SFGenericTask, SFRestResourceTask
+ *
+ * \author Livan Yi Du
+ */
 class SFNetworkAccessTask : public sf::SFGenericTask {
 	Q_OBJECT
-public slots:
+private slots:
 	void onReplyFinished();
 	void onNetworkTimeout();
 signals:
-	void taskWillRetry();
-	void taskDidRetry();
+	void taskWillRetry(); /*!< Emitted before the task re-start itself. */
+	void taskDidRetry(); /*!< Emitted after the task re-start itself. */
 
 public:
+	/*! @param networkAccessManager the shared QNetworkAccessManager instance */
 	SFNetworkAccessTask(QNetworkAccessManager * const networkAccessManager);
+	/*! @param networkAccessManager the shared QNetworkAccessManager instance
+	 * @param request a instance of QNetworkRequest
+	 * @param method the HTTP verb */
 	SFNetworkAccessTask(QNetworkAccessManager * const networkAccessManager, const QNetworkRequest & request, const HTTPMethodType & method = HTTPMethod::HTTPGet);
+	/*! @param networkAccessManager the shared QNetworkAccessManager instance
+	 * @param url the absolute URL of your network task. HTTP or HTTPS
+	 * @param method the HTTP verb */
 	SFNetworkAccessTask(QNetworkAccessManager * const networkAccessManager, const QUrl & url, const HTTPMethodType & method = HTTPMethod::HTTPGet);
+	/*! @param networkAccessManager the shared QNetworkAccessManager instance
+	 * @param path the absolute path of the URL. Should start with HTTP or HTTPS
+	 * @param method the HTTP verb */
 	SFNetworkAccessTask(QNetworkAccessManager * const networkAccessManager, const QString & path, const HTTPMethodType & method = HTTPMethod::HTTPGet);
+	/*! Default destructor */
 	virtual ~SFNetworkAccessTask();
 
-	Q_INVOKABLE virtual void startTaskAsync(QObject* resultReceiver = NULL, const char * resultReceiverSlot = NULL);
+	/*! Start the task asynchronously.
+	 * @param resultReceiver The object that handles the result
+	 * @param resultReceiverSlot The slot description string. Should be a string generated using macro SLOT() */
+	Q_INVOKABLE void startTaskAsync(QObject* resultReceiver = NULL, const char * resultReceiverSlot = NULL);
 
 	/* Accessors */
+	/*! @return the @c QNetworkRequest instance */
 	QNetworkRequest & request() { return this->mRequest;};
 
+	/*! @return whether the network task should try to use the cache installed in @c QNetworkAccessManager. @see setUserCache() */
 	bool isUseCache() { return this->mUseCache;};
+	/*! Set whether the network task should try to use cache. If cache is not configured in given @c QNetworkAccessManager instance,
+	 * setting this flag takes no effect.
+	 * @param useCache whether use cache */
 	void setUseCache(bool useCache) { this->mUseCache = useCache;};
 
+	/*! @return the amount of time in milliseconds that the task should wait for, before canceling the network transaction */
 	unsigned long int networkTimeout() {return this->mNetworkTimeout;};
+	/*! Set the amount of time in milliseconds that the task should wait for, before canceling the network transaction */
 	void setNetworkTimeout(unsigned long int msec) { this->mNetworkTimeout = msec;};
 
+	/*! Set additional attributes to the network request
+	 * @param code the code of the attribute.
+	 * @param value the value of the attribute. */
 	void setRequestAttribute(QNetworkRequest::Attribute code, const QVariant & value) {mRequest.setAttribute(code, value);};
+	/*! @return the attribute associated with given attribute code */
 	QVariant requestAttribute(QNetworkRequest::Attribute code) {return mRequest.attribute(code);};
 
+	/*! @return the bytes array that will be sent as request's content */
 	QByteArray requestBytesArray() { return this->mRequestBytesArray;};
-	/* passing non-null bytes array will clear any QIODevice assigned earlier via "setRequestData" */
+	/*! Set the bytes array that will be sent as request's content. Passing non-null bytes array will clear any
+	 * QIODevice assigned earlier via @c setRequestData() */
 	void setRequestBytesArray(const QByteArray & bytes);
 
+	/*! @return the @c QIODevice that contains the request's content data */
 	QIODevice *requestData() { return this->mRequestData;};
-	/* this function takes ownership. Passing non-null QIODevice will clear any QByteArray assigned earlier via "setRequestBytesArray" */
+	/*! Set a @c QIODevice that contains data to sent as request's content. This function takes ownership. Passing non-null
+	 * @c QIODevice will clear any @c QByteArray assigned earlier via @c setRequestBytesArray() */
 	void setRequestData(QIODevice * data);
 
 protected:
+	/*! Possible states of the task */
 	enum NetworkTaskState {
-		StateNotStarted,
-		StateReadyToSend,
-		StateWaiting,
-		StateHasResponse,
-		StateNeedToRedirect,
-		StateReadyToProcess,
-		StateProcessing,
-		StateFinished,
-		StateError,
-		StateNeedToRetry,
+		StateNotStarted, /*!< The task is not started yet. */
+		StateReadyToSend, /*!< The request of this task is ready to be sent. */
+		StateWaiting, /*!< The task is waiting for network response. */
+		StateHasResponse, /*!< The task has received a response from server. */
+		StateNeedToRedirect, /*!< The task need to re-sent a request with different URL. */
+		StateReadyToProcess, /*!< The task has a response that is ready to be processed. */
+		StateProcessing, /*!< The task is processing the response (in a separate thread). */
+		StateFinished, /*!< The task is finished without error. */
+		StateError, /*!< The task is finished with error, re-try is not an option. */
+		StateNeedToRetry, /*!< The task is finished with error. It will re-try later. */
 	};
 
-	QNetworkAccessManager *mNetworkAccessManager;
-	QNetworkRequest mRequest;
-	HTTPMethodType mMethod;
-	QByteArray mRequestBytesArray;
-	QIODevice *mRequestData;
-	NetworkTaskState mState;
-	QNetworkReply *mCurrentReply;
-	bool mUseCache;
-	unsigned long int mNetworkTimeout;
-	QTimer *mNetworkTimer;
+	QNetworkAccessManager *mNetworkAccessManager; /*!< Holds a shared instance of @c QNetworkAccessManager */
+	QNetworkRequest mRequest; /*!< Holds a instance of @c QNetworkRequest */
+	HTTPMethodType mMethod; /*!< Holds the HTTP verb of the request */
+	QByteArray mRequestBytesArray; /*!< Holds the data of request's content */
+	QIODevice *mRequestData; /*!< Holds a @c QIODevice which contains the data of request's content */
+	NetworkTaskState mState; /*!< Current state of the task */
+	QNetworkReply *mCurrentReply; /*!< Current @c QNetworkReply instance */
+	bool mUseCache; /*!< Whether should use cache if possible */
+	unsigned long int mNetworkTimeout; /*!< The network timeout in milliseconds */
+	QTimer *mNetworkTimer; /*!< Holds a timer for current executing network  */
 
-	virtual void run();
+	/*! Main entry point for separate thread execution. Subclass should not call this function directly. */
+	void run();
+	/*! Inherited from base class, not used in current implementation. Subclass should not call this function directly. */
+	TaskStatus execute() {throw std::exception(); };
+	/*! Called before the task do any actual work. Subclass should not call this function directly.
+	 * Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
 	virtual void prepare();
-	virtual TaskStatus execute() {throw std::exception(); };
+	/*! Called at the end of task. This function is responsible to cleanup memory, attempt re-try and emit signals. Subclass should not call this function directly.
+	 * Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
 	virtual void cleanup();
+	/*! Responsible to reset the task. Subclass should not call this function directly.
+	 * Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
 	virtual bool retry();
 
+	/*! Prepare and validate current request
+	 * @return The state of the task:
+	 * 	+ @c SFNetworkAccessTask::StateReadyToSend indicates that the request object is fully prepared and ready to be sent.
+	 * 	+ @c SFNetworkAccessTask::StateError indicates that the request object is not ready and the operation should be aborted.
+	 *
+	 * Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
+	virtual NetworkTaskState ensureRequest();
+
+	/*! Pre-porcess the reply and handles HTTP redirect.
+	 * @return The state of the task:
+	 * 	+ @c SFNetworkAccessTask::StateReadyToProcess indicates that the reply is ready to be processed.
+	 * 	+ @c SFNetworkAccessTask::StateNeedToRedirect indicates that another request to server is needed. The new URL should be assigned to
+	 * 	@c SFNetworkAccessTask::mRequest when the function returns.
+	 * 	+ @c SFNetworkAccessTask::StateError indicates that the error is detected and the task should be abort.
+	 *
+	 * 	Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
+	virtual NetworkTaskState preprocessReply(QNetworkReply * reply);
+
+	/*! Process the reply. This function is always called in separate thread.
+	 * @return The state of the task:
+	 * 	+ @c SFNetworkAccessTask::StateFinished indicates that the task is finished and the result is ready for delivery
+	 * 	+ @c SFNetworkAccessTask::StateError indicates that the error is detected and the task should be abort.
+	 * 	+ @c SFNetworkAccessTask::StateNeedToRetry indicates that error is detected and task should be restarted. Custom logic should check
+	 * 	whether the re-try is enabled. It shouldn't return this state if re-try is not enabled.
+	 *
+	 * 	Please see @ref SFNetworkAccessTask_Subclass "Subclassing Notes" for more details. */
+	virtual NetworkTaskState processReply(QNetworkReply * reply);
+
+	//debug
+	/*! Convenient function for debug purpose. Generate a nice formated summary of given reply's headers */
+	QString composeReplyHeader(QNetworkReply * reply);
+	/*! Convenient function for debug purpose. Generate a string expression of given state */
+	QString stateDescription(NetworkTaskState state);
+
+private:
 	Q_INVOKABLE void moveQObjectsToThread(QThread *thread);
 	Q_INVOKABLE void fsmDispatcher();
 	NetworkTaskState initiateNetworkAccess();
 	QNetworkReply * createReplyAndExit();
-
-	virtual NetworkTaskState ensureRequest();
-	virtual NetworkTaskState preprocessReply(QNetworkReply * reply);
-	virtual NetworkTaskState processReply(QNetworkReply * reply);
-
-	//debug
-	QString composeReplyHeader(QNetworkReply * reply);
-	QString stateDescription(NetworkTaskState state);
 
 };
 //typedef sf::SFNetworkAccessTask::HTTPMethod HTTPMethod;
